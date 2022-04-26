707b33b2374fb8f5e943a85b0df0c8391992e322
KRIZ-SER <92659288+KRIZ-SER@users.noreply.github.com>
2022-04-26 18:12:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Change owner n
 global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit 
 
-global.repox = 'raselcomel/mymd'
+global.repox = 'kriz/mymd'
 global.branx = 'master'
 
 global.APIs = { // API Prefix
@@ -46,7 +46,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = ''
-  var sticker_author = '               ig\n🔥 @rasel.ganz 🔥'
+  var sticker_author = '               ig\n🔥 @_krishna.x._ 🔥'
  } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -68,7 +68,7 @@ const cpack = fs.readFileSync("lib/colong.json")
 const ctickerpack = JSON.parse(cpack)
 if (ctickerpack.cpackname == '') {
   var colong_name = 'sikilinsut olang nylong stikell'
-  var colong_author = 'Ciss 📸\n\n@rasel.ganz'
+  var colong_author = 'Ciss 📸\n\n@_krishna.x._'
  } else {
   var colong_name = ctickerpack.cpackname
   var colong_author = ctickerpack.cauthor
@@ -85,28 +85,28 @@ global.pclg = colong_name
 global.aclg = colong_author
 //======================================//
 
-global.wm = '@rasel.ganz'
+global.wm = '@_krishna.x._'
 global.ucpn = ''
 
 //================= Url =================//
-global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
-global.linkig = 'https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link'
+global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWmJLJwJgPQIaKM'
+global.linkig = 'https://www.instagram.com/p/CH1A1c9JpY/?utm_medium=copy_link'
 global.linkyt = ''
-global.linkfb = 'https://fb.watch/aYv0jAffAO'
-global.web = 'https://raselcomel.github.io'
-global.web1 = 'https://anu.rasell.repl.co'
+global.linkfb = 'https://fb.watch/aY0jAffAO'
+global.web = 'https://kriz.github.io'
+global.web1 = 'https://anu.kriz.repl.co'
 
 global.deslink = ''
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@rasel.ganz'
+global.bodlink = '_krishna.x._'
 
 //========= Url Template Button =========//
 global.dtu = 'Group Bot WhatsApp'
-global.urlnya = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
+global.urlnya = 'https://chat.whatsapp.com/CUCsW6BfmJLJwJgPQIaKM'
 
 //========= Call Template Button =========//
-global.dtc = 'Add Me'
-global.nmbrnya = '+62 822-5604-8971'
+global.dtc = 'owner'
+global.nmbrnya = '919633687665'
 
 
 //============ Proses Bot ============//
